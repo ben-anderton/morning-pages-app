d444f5dc7aecb1de26e7b7a7657f524fa2781091
ben-anderton <benanderton52@gmail.com>
2026-07-22 23:16:13 -0500

    Add func to update Daily Page with new content
    Add basic update functionality but needs to add
    additional functionality to make the basic app
    work as intended.

--- script.js
+++ script.js
@@ -2,9 +2,38 @@
 
 
 // Pseudocode for Daily Update Functionality
-// 1. Have a function getTime
+// 1. Have a function get today's date
 // 2. Reference Database(Array of Data)
 // 3. Update Current Page Info (Read, Ask, Pray) with Today's info
 //      IF Today's date matched Array value, then return today's value
 //      ELSE IF Today's date !=== Array's Date(No Data exists for today) break
 // 4. Always run the function on refresh.
+
+
+
+// Daily Update
+
+const today = new Date();
+console.log(today.toLocaleDateString('en-US')); 
+
+const dateHeader = document.getElementById("date-header");
+const scriptureVerses = document.getElementById("scripture-verses");
+const askBoxOne = document.getElementById("ask-box-one");
+const askBoxTwo = document.getElementById("ask-box-two");
+const prayBox = document.getElementById("pray-box");
+
+let dailyPrompts = [
+    [],
+    [],
+    [],
+]
+
+function updateTodayPage() {
+    dateHeader.textContent = today.toLocaleDateString('en-US', {month: 'long', day: 'numeric'});
+    scriptureVerses.textContent = "This will be replaced by an array or Database entry in futureeee";
+    askBoxOne.textContent = "How do you do?";
+    askBoxTwo.textContent = "When will this project make progress?";
+    prayBox.textContent = "Thank you Jesus, Amen!";
+
+    console.log("Function updateTodayPage completed");
+}
