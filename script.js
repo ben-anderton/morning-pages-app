6e3730fb6615f679ecbd86cc307428fa177588f2
ben-anderton <benanderton52@gmail.com>
2026-07-22 22:49:32 -0500

    Add script.js and pseudocode for basic daily page

---
+++ script.js
@@ -0,0 +1,10 @@
+// This is the Core Scripts for Morning Pages App
+
+
+// Pseudocode for Daily Update Functionality
+// 1. Have a function getTime
+// 2. Reference Database(Array of Data)
+// 3. Update Current Page Info (Read, Ask, Pray) with Today's info
+//      IF Today's date matched Array value, then return today's value
+//      ELSE IF Today's date !=== Array's Date(No Data exists for today) break
+// 4. Always run the function on refresh.
