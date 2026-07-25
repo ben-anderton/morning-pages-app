423f296d503ef6819128fa3425c5e65ef8f116c3
ben-anderton <benanderton52@gmail.com>
2026-07-25 09:37:04 -0500

    Add function to return today's prompt

--- script.js
+++ script.js
@@ -22,15 +22,25 @@ const askBoxOne = document.getElementById("ask-box-one");
 const askBoxTwo = document.getElementById("ask-box-two");
 const prayBox = document.getElementById("pray-box");
 
-// The array below wil be used as a temporary database for storing the miscellaneous prompts for each day.
+// The arrays below wil be used as a temporary database for storing the miscellaneous prompts for each day.
 // Decision: Should each unique day have its own array or should one large array be appended with new entries.
 //  Until NODE.JS backend is working -> Unique arrays for each day
 
 let dailyPrompts = [
-    [],
-    [],
-    [],
+    {date: "7/24/2026", dateHeaderString: "July 24", verse: "Scripture Verses", questionA: "Question 1", questionB: "Question 2", prayer: "Prayer"},
+    {date: "7/25/2026", dateHeaderString: "July 25", verse: "Scripture Verses", questionA: "Question 1", questionB: "Question 2", prayer: "Prayer"},
+    {date: "7/26/2026", dateHeaderString: "July 26", verse: "Scripture Verses", questionA: "Question 1", questionB: "Question 2", prayer: "Prayer"},
 ]
+
+function getTodayPrompt(){
+    const todayDateString = today.toLocaleDateString('en-US');
+    console.log(todayDateString);
+
+    //return all objects in dailyPrompts array for today
+    let todayPrompt = dailyPrompts.find(dayPrompt => dayPrompt.date === todayDateString);
+    return(todayPrompt);
+    console.log(todayPrompt);
+}
 
 // Basic func to update DailyPage until NODE.JS database is created
 
@@ -46,3 +56,9 @@ function updateTodayPage() {
 
 //  Create function below to convert date into string and compare with the name of array. 
 //  This will allow the call to reference the correct array above.
+
+//      Pseudocode for updateTodayPage
+//  1. get today's date
+//  2. if todayString = arr[0] (for loop?)
+//  3. call the variable for today's date
+//  4. take the variable array and plug in to html
