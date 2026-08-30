b13a2257fc8ea19e3cc9f2b7901941f84b048905
ben-anderton <benanderton52@gmail.com>
2026-08-29 20:54:29 -0500

    Add UI update to Sign In/Out actions

--- public/app.js
+++ public/app.js
@@ -13,3 +13,18 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}! </h3>`
+    } else {
+        //not signed In
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
